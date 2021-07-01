7d4d49c78262cf220fadb48f78a2d61299e11fc7
Minal <patilminalp@gmail.com>
2021-07-02 01:10:55 +0530

    Added:create a stack

--- src/com/stackqueue/StackMain.java
+++ src/com/stackqueue/StackMain.java
@@ -3,7 +3,15 @@ package com.stackqueue;
 public class StackMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Stack and Queue Problem....!!!");
+		Stack stack = new Stack();
+		
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		
+		stack.print();
 
 	}
 
